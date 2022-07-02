6cde090c7cd0df62ee35d60d3ba521f1205abff2
TheMeq <richard.sparrow@hotmail.co.uk>
2022-07-02 18:05:56 +0100

    Add Colours to Embeds.

--- CountryBot/Embeds/BotEmbeds.cs
+++ CountryBot/Embeds/BotEmbeds.cs
@@ -1,6 +1,5 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
-using System.Net.NetworkInformation;
 using CountryBot.Models;
 using Discord;
 
@@ -15,7 +14,8 @@ internal static class BotEmbeds
             .WithDescription(listOfCountries
                 .Aggregate(string.Empty,
                     (current, country) => 
-                        current + $"{country.Country} - {country.Alpha2} or {country.Alpha3}\r\n"));
+                        current + $"{country.Country} - {country.Alpha2} or {country.Alpha3}\r\n"))
+            .WithColor(new Color(0x3BA55D));
     }
 
     public static EmbedBuilder InvalidCountryCode(CountryModel tryThis = null)
@@ -25,45 +25,44 @@ internal static class BotEmbeds
             .WithDescription(
                 tryThis == null 
                     ? null 
-                    : $"Did you mean: ``/set {tryThis.Alpha2}`` for {tryThis.Country}?");
+                    : $"Did you mean: ``/set {tryThis.Alpha2}`` for {tryThis.Country}?")
+            .WithColor(new Color(0xED4245));
     }
 
     public static EmbedBuilder CountrySet(CountryModel country)
     {
         return new EmbedBuilder()
-            .WithTitle($"Your country has been set to {country.Country} on this guild!");
+            .WithTitle($"Your country has been set to {country.Country} on this guild!")
+            .WithColor(new Color(0x3BA55D));
     }
 
     public static EmbedBuilder CountryRemoved()
     {
         return new EmbedBuilder()
-            .WithTitle("Your country role has been removed on this guild.");
+            .WithTitle("Your country role has been removed on this guild.")
+            .WithColor(new Color(0x3BA55D));
     }
 
     public static EmbedBuilder NotInDms()
     {
         return new EmbedBuilder()
             .WithTitle("Don't do the commands here!")
-            .WithDescription("These commands are guild specific, so the command has to be done in the guild you want to set or remove the role on.");
+            .WithDescription("These commands are guild specific, so the command has to be done in the guild you want to set or remove the role on.")
+            .WithColor(new Color(0xED4245));
     }
 
     public static EmbedBuilder NotInCountry()
     {
         return new EmbedBuilder()
-            .WithTitle("You are not in a country role on this guild.");
+            .WithTitle("You are not in a country role on this guild.")
+            .WithColor(new Color(0xFAA81A));
     }
 
     public static EmbedBuilder NoSearchResults(string searchQuery)
     {
         return new EmbedBuilder()
             .WithTitle($"Search Results for '{searchQuery}'.")
-            .WithDescription("No Results Found.");
-    }
-
-    public static EmbedBuilder SetGlobalColour(this EmbedBuilder inputEmbedBuilder)
-    {
-        
-        return inputEmbedBuilder
-            .WithColor(new Color(0x3BA55D));
-    }
+            .WithDescription("No Results Found.")
+            .WithColor(new Color(0xFAA81A));            
+    }    
 }
