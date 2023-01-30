35cd085bb36927ca7d74fd7f84e0e39434d075e6
TheMeq <richard.sparrow@hotmail.co.uk>
2023-01-30 19:18:40 +0000

    removed unused usings

--- CountryBot/Program.cs
+++ CountryBot/Program.cs
@@ -11,7 +11,6 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using static Discord.GatewayIntents;
 using CountryBot.Models;
-using Discord.Rest;
 
 namespace CountryBot;
 internal class Program
